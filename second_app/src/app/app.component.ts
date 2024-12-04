ccb6f106fb42ad4ef516431afbba83e8c5af9451
cbseirani <34148393+cbseirani@users.noreply.github.com>
2024-12-04 11:28:06 -0500

    creating dynamic list of users and their tasks

--- second_app/src/app/app.component.ts
+++ second_app/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from './tasks/tasks.component';
-
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
@@ -14,16 +13,13 @@ import { DUMMY_USERS } from './dummy-users';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUser = DUMMY_USERS[0];
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)
+  }
 
   onSelectUser(id: string){
-    console.log('selected user: ' + id);
-    var user = DUMMY_USERS.find(user => user.id === id);
-    if(user){
-      this.selectedUser = user;
-    }
-    else{
-      console.log('User not found!');
-    }
+    this.selectedUserId = id;
   }
 }
