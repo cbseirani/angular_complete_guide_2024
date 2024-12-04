bd6ff8d8e708d80c552cedc35e7ab81ac8d96824
cbseirani <34148393+cbseirani@users.noreply.github.com>
2024-12-04 09:42:30 -0500

    adding list of users and a new tasks component

--- second_app/src/app/app.component.ts
+++ second_app/src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { TasksComponent } from './tasks/tasks.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUser = DUMMY_USERS[0];
+
+  onSelectUser(id: string){
+    console.log('selected user: ' + id);
+    var user = DUMMY_USERS.find(user => user.id === id);
+    if(user){
+      this.selectedUser = user;
+    }
+    else{
+      console.log('User not found!');
+    }
+  }
+}
