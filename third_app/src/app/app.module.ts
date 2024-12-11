9e20b922bdf502a48de885a4bc46b52f143a72a9
cbseirani <34148393+cbseirani@users.noreply.github.com>
2024-12-10 19:18:10 -0500

    migrate all components to modules

--- third_app/src/app/app.module.ts
+++ third_app/src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
-import { BrowserModule } from "@angular/platform-browser";
+import { CardComponent } from "./shared/card/card.component";
+import { TaskComponent } from "./tasks/task/task.component";
+import { AddTaskComponent } from "./tasks/add-task/add-task.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent, 
+        HeaderComponent, 
+        UserComponent, 
+        CardComponent, 
+        TasksComponent, 
+        TaskComponent, 
+        AddTaskComponent],
     bootstrap: [AppComponent],
-    imports: [HeaderComponent, UserComponent, TasksComponent, BrowserModule]
+    imports: [BrowserModule, FormsModule]
 })
 export class AppModule {}
--- third_app/src/app/tasks/tasks.component.ts
+++ third_app/src/app/tasks/tasks.component.ts
@@ -1,12 +1,8 @@
 import { Component, Input, inject } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
-import { AddTaskComponent } from './add-task/add-task.component';
 import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
